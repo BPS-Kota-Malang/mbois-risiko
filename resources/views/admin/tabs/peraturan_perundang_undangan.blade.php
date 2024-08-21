3815f58cf1db7db7f1c2412facea7b0ca2d680cc
Watashiell <dzellyas@gmail.com>
2024-08-21 15:24:29 +0700

    Hapus Tombol Refresh Perundangan

--- resources/views/admin/tabs/peraturan_perundang_undangan.blade.php
+++ resources/views/admin/tabs/peraturan_perundang_undangan.blade.php
@@ -8,11 +8,11 @@
                     class="px-4 py-2 mb-2 bg-blue-500 rounded-md text-white font-medium tracking-wide hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition duration-300">
                     Tambah
                 </button>
-                <!-- Refresh button -->
+                {{-- <!-- Refresh button -->
                 <button onclick="refreshTable()"
                     class="px-4 py-2 mb-2 bg-blue-500 rounded-md text-white font-medium tracking-wide hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition duration-300">
                     Refresh
-                </button>
+                </button> --}}
                 <!-- Table -->
                 <table class="min-w-full divide-y divide-gray-200">
                     <thead class="bg-gray-50">
