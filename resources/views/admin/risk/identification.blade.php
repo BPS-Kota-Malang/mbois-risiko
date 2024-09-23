6195af505003587d44448a90bcdd112ff52f7a5d
praabima <100179685+praabima@users.noreply.github.com>
2024-09-23 10:35:52 +0700

    filter identifikation

--- resources/views/admin/risk/identification.blade.php
+++ resources/views/admin/risk/identification.blade.php
@@ -24,6 +24,7 @@
                         @endforeach
                     </select>
                 </div>
+                <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded">Filter</button>
             </form>
         </div>
     </div>
